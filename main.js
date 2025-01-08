71c507ef3eeb223e0d4bad220226190a9f28b07b
Anurag Joshi <anuragj990@gmail.com>
2025-01-08 16:06:17 +0530

    1st try

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+
+  res.end("Hello, world!");
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log("server has started listening")
 });
 
 // DO NOT EDIT BELOW THIS LINE
